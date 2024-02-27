1b739c546147c4177d9b88a5ed0c0b8e6e78eb9f
JR1b <joao.rib.work@gmail.com>
2024-02-27 12:21:28 +0000

    feat: render ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
@@ -8,7 +8,22 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container mt-3">
+      <AppProvider>
+        <h1 className='mb-3'>Shopping App</h1>
+        <div className='row'>
+          <div className='col'>
+            <Location />
+          </div>
+          <div className='col'>
+            <CartValue />
+          </div>
+        </div>
+        <h2>Shopping Cart</h2>
+        <ExpenseList />
+        <h2>Add Items</h2>
+        <ItemSelected />
+      </AppProvider>
     </div>
   );
 }
